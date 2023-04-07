cd7e196710970705c1d2b54effbe3777ecde28f3
Natasha <puzir-125@mail.ru>
2023-04-08 01:36:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 2090817777
+chat_id = 1012233839
 
 def solution(x: np.array) -> float:
     n = len(x)
     time = 10
-    errors = np.random.exponential(scale=1, size=n) - 23
+    errors = np.random.exponential(scale=1, size=n) - 39
     v_real = x - errors
     a = np.mean(v_real) / time
     return a
